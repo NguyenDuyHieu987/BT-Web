d0f4af0a8a1240899af1a775d9a1f9a8103c9e62
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-10-05 19:12:06 +0700

    add

---
+++ admin/posts_connect.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "th02_web_nc");
+mysqli_set_charset($conn, 'UTF8');
---
+++ connect.php
@@ -0,0 +1,2 @@
+<?php $conn = mysqli_connect("localhost", "root", "", "th02_web_nc");
+mysqli_set_charset($conn, 'UTF8');
