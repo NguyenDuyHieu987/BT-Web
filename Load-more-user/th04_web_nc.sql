84a982a0d7984b349fdc98f154254af05afa874d
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-10-10 19:44:08 +0700

    add

---
+++ Load-more-user/th04_web_nc.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2022 at 02:41 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `th04_web_nc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_customer`
+--
+
+CREATE TABLE `tb_customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tb_customer`
+--
+
+INSERT INTO `tb_customer` (`id`, `name`, `website`) VALUES
+(1, 'Nguyễn Văn Cường', 'freetuts.net'),
+(2, 'Trương Phúc Hoài Minh', 'freetuts.net'),
+(3, 'Đặng Văn Chương', 'freetuts.net'),
+(4, 'Trương Tấn Thành', 'freetuts.net'),
+(5, 'Lâm văn Lang', 'demo.com'),
+(6, 'Nguyễn Văn Kiệt', 'ajax.com'),
+(7, 'Nguyễn Thị Nở', 'thimau.com'),
+(8, 'Đặng Thị Thoa', 'scrolling.com'),
+(9, 'Trương Văn Kiệt', 'ajaxscrolling.com'),
+(10, 'Đặng Thị Tâm', 'nono.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_customer`
+--
+ALTER TABLE `tb_customer`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_customer`
+--
+ALTER TABLE `tb_customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
