588b9a113f7e6b8028a6a5538290ed2959748cc0
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-10-29 10:02:15 +0700

    update split page - ajax

--- RazorPagesMovie/RazorPagesMovie/Pages/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace RazorPagesMovie.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPostGetAjax(string name)
+        {
+            return new JsonResult("Hello " + name);
         }
     }
 }
--- RazorPagesMovie/RazorPagesMovie/wwwroot/js/site.js
+++ RazorPagesMovie/RazorPagesMovie/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('#callAjax').click(function () {
+  $.ajax({
+    type: 'POST',
+    url: '/Index?handler=GetAjax',
+    data: { name: 'Mark' },
+    contentType: 'application/x-www-form-urlencoded',
+    dataType: 'json',
+    headers: {
+      RequestVerificationToken: $(
+        'input:hidden[name="__RequestVerificationToken"]'
+      ).val(),
+    },
+    success: function (msg) {
+      alert(msg);
+    },
+  });
+});
